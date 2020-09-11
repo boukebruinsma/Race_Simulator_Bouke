cf620cc82bad82b75c2a07e827488f23f02b3201
bouke <boukebruinsmadalfsen@gmail.com>
2020-09-11 15:02:37 +0200

    Initial check-in of module Race_Simulator

---
+++ Race_Simulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Race_Simulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
